f4095367dc78a27cb889dcc2d4754364c3fde7e0
Ally Netzel <netzelally@gmail.com>
2018-12-21 01:58:17 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -5,7 +5,7 @@ class SongsWorker
  
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
-      Song.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 end
