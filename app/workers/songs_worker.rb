b2b02d311b73f83935db7b7551f704befcfa2a28
Ally Netzel <netzelally@gmail.com>
2018-12-21 01:57:23 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,8 +4,8 @@ class SongsWorker
   include Sidekiq::Worker
  
   def perform(songs_file)
-    CSV.foreach(leads_file, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    CSV.foreach(songs_file, headers: true) do |song|
+      Song.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
   end
 end
